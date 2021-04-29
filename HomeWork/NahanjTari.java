379f8ca7dca4a8ca87e12974a7feb25ba135cfae
Vahan78 <SHIELD78@yandex.ru>
2021-04-29 20:34:58 +0400

    homework

---
+++ HomeWork/NahanjTari.java
@@ -0,0 +1,15 @@
+package com.company.Day4.HomeWork;
+
+import java.util.Scanner;
+
+public class NahanjTari {
+    public static void main(String[] args) {
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("input Year");
+//        int inputYear = scanner.nextInt();
+//if I
+
+    }
+}
